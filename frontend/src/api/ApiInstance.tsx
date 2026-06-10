e442ffdd0c8de97c39a5642e432292d29f051a79
unknown <alexpeteronoja@gmail.com>
2026-06-10 22:10:49 +0100

    updated

--- frontend/src/api/ApiInstance.tsx
+++ frontend/src/api/ApiInstance.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const api = "http://localhost:3000/api/v1"; // "https://courier-service-app-he8z.onrender.com/api/v1"; ;
+const api = "https://courier-service-app-he8z.onrender.com/api/v1";
 
 const ApiInstance = axios.create({
   baseURL: api,
