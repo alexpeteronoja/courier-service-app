62fdf9fee5e6d3f3112f55a21feb88dba7bf7fca
unknown <alexpeteronoja@gmail.com>
2026-06-13 15:14:24 +0100

    handled error ouput

--- backend/src/controllers/errorController.js
+++ backend/src/controllers/errorController.js
@@ -1,6 +1,37 @@
-// handle JWT error
-
 import { AppError } from '../utils/appError.js';
+
+// handle invalid ID
+const handleCastErrorDB = (err) => {
+  const message = `Invalid ${err.path}: ${err.value}`;
+  return new AppError(message, 400);
+};
+
+// handle duplicate unique value error
+
+const handleDuplicateFieldsDB = (err) => {
+  const value = err.errorResponse.errmsg.match(/(["'])(\\?.)*?\1/)[0];
+
+  const message = `Duplicate fields value: ${value}. Please use another value!`;
+
+  // easier method
+
+  // const field = Object.keys(err.keyValue)[0];
+  // const value = Object.values(err.keyValue)[0];
+  // const message = `Duplicate field: ${field} (${value}). Please use another value!`;
+
+  return new AppError(message, 400);
+};
+
+// handle validation error
+
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
+
+// handle JWT error
 
 const handleJWTError = (_err) =>
   new AppError('Invalid token. Please login again', 401);
@@ -53,6 +84,9 @@ export const errorController = (err, req, res, next) => {
   } else if (process.env.NODE_ENV === 'production') {
     let error = err;
 
+    if (err.name === 'CastError') error = handleCastErrorDB(error);
+    if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+    if (err.name === 'ValidationError') error = handleValidationErrorDB(error);
     if (err.name === 'JsonWebTokenError') error = handleJWTError(error);
     if (err.name === 'TokenExpiredError') error = handleJWTExpiredError(error);
 
